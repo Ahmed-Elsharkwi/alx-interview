7a8613500a11bffd96f8173eaf82ca17164c2036
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-05-30 20:44:31 +0300

    MM

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+"""
+solve the problem of pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    calculate the pascal triangle by calculating the sum of two pervious variable to get the value
+    of a variable in the next list and also add 1 to each side of the list
+    """
+    main_list = []
+
+    if n > 0:    
+        for row in range(0, n):
+            internal_list = []
+
+            if row != 0:
+                internal_list.append(1)
+                for j in range(1, row):
+                    internal_list.append(main_list[row-1][j-1] + main_list[row-1][j])
+
+            internal_list.append(1)
+            main_list.append(internal_list)
+
+    return main_list
