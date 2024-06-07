8884f581b0f8a7ae4358635581d44df47c1742bf
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-07 16:30:41 +0300

    MM

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+check if all boxes are opened or not
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    we are gonna check all if all boxes
+    can be opened or not by opening each
+    box and get all the keys from the box
+    after that we are gonna use those keys
+    to open other boxes after we finish all
+    key in our set and there are some boxes
+    which are not open the function will return
+    False but if we opened all boxes using the keys
+    in the set the function will return True
+    """
+    num_boxes = len(boxes) - 1
+    length = num_boxes + 1
+
+
+    while True:
+        if num_boxes != 0 and boxes[0]:
+            element = boxes[0].pop(0)
+        else:
+            break
+
+        if element == None:
+            num_boxes -= 1
+            continue
+
+        if element != 0 and element != length and boxes[element] != 1:
+            if len(boxes[element]) == 0:
+
+                boxes[0].append(None)
+            else:
+                for ele in boxes[element]:
+                    boxes[0].append(ele)
+            boxes[element] = 1
+            num_boxes -= 1
+
+
+    if num_boxes == 0:
+        return True
+    else:
+        return False
---
+++ 0x01-lockboxes/test.py
@@ -0,0 +1,7 @@
+list = [[]]
+list[0].append(None)
+print(list[0])
+if list[0]:
+    print("i am good")
+else:
+    print("Fuck you")
