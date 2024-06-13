8b4f39318390bd26b58e23ac3f398d0f6bbfac40
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-13 15:41:47 +0300

    solve the problem

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,7 +5,7 @@ to reach the number of n which is the number of letter H
 """
 
 
-def fun(n, copy, paste, num_opt):
+def fun(n, copy, paste, num_opt, new_list):
     """ return a number of operations """
     opt_2 = 0
     if paste > n:
@@ -13,11 +13,14 @@ def fun(n, copy, paste, num_opt):
     if paste == n:
         return num_opt
 
-    copy_1 = paste
-    opt_1 = fun(n, copy_1, paste + copy_1, num_opt + 2)
+    if new_list[paste] is None:
+        copy_1 = paste
+        opt_1 = fun(n, copy_1, paste + copy_1, num_opt + 2, new_list)
+    else:
+        opt_1 = new_list[paste]
 
     if paste != 1:
-        opt_2 = fun(n, copy, paste + copy, num_opt + 1)
+        opt_2 = fun(n, copy, paste + copy, num_opt + 1, new_list)
 
     if opt_1 != 0 and opt_2 != 0:
         return min(opt_1, opt_2)
@@ -31,5 +34,5 @@ def minOperations(n):
     """ return the fewest number of the operations """
     if n == 0 or n == 1 or type(n) != int:
         return 0
-
-    return fun(n, 1, 1, 0)
+    new_list = [None] * (n + 1)
+    return fun(n, 1, 1, 0, new_list)
