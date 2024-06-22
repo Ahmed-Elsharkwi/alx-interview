01360c206320f398fda5edb82e8f32cc63ff2c0a
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-22 15:38:45 +0300

    MM

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,21 +19,21 @@ def main():
     """ main function """
     size = 0
     states_code = {
-        200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+        "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     counter = 1
     try:
         for line in sys.stdin:
             elements = line.split(" ")
             res = re.search(
-                r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-                r'\s\-\s\[\d{4}\-\d{2}\-\d{2}\s\d{2}\:'
+                r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\w+)'
+                r'\s?-\s?\[\d{4}\-\d{2}\-\d{2}\s\d{2}\:'
                 r'\d{2}\:\d{2}\.\d+\]\s\"GET\s/projects/260\sHTTP/1.1\"'
-                r'\s\d{1,3}\s\d{1,4}$', line)
+                r'\s(\d{1,3}|\w+)\s\d{1,4}$', line)
             if res is not None:
-                size += int(elements[8])
-                number = int(elements[7])
+                size += int(elements[-1])
+                number = elements[-2]
                 if number in states_code:
-                    states_code[int(elements[7])] += 1
+                    states_code[number] += 1
 
                 if counter == 10:
                     happen(size, states_code)
---
+++ 0x03-log_parsing/test.py
@@ -0,0 +1,2 @@
+list = [1, 3, 5, 7]
+print(list[-1], list[-2])
