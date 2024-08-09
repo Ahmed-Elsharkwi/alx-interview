7ee94ac1a5c74b980788645f6ab800e270506dd0
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-08-09 14:21:25 +0300

    solve the problem

--- 0x0A-primegame/try.py
+++ 0x0A-primegame/try.py
@@ -1,17 +1,10 @@
-def check_the_prime_number(number):
+import random
+number_list = []
+def check_the_prime_number(number_list):
     """
     it will check if the number is prime or not
     """
-    if number == 1:
-        return 0;
-    counter = 2
-    number_factors = 0
-
-    while (counter <= int(number / 2)):
-        if number_factors > 1:
-            return 0
-        if number % counter == 0:
-            number_factors += 1
-        counter += 1
-    return 1
-print(check_the_prime_number(3))
+    for i in range(0, 100):
+        number_list.append(random.randint(0, 100))
+check_the_prime_number(number_list)
+print(number_list)
